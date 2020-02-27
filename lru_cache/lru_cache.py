b36c499105fbcb2b0afa0037771c474745a36d46
Amira Adediran <amira@Amiras-MacBook-Pro.local>
2020-02-27 13:47:55 +0100

    Add DLL for lru cache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,7 +1,4 @@
-import sys
-sys.path.append('../doubly_linked_list')
-from doubly_linked_list import DoublyLinkedList
-
+from doubly_linked_list import DoublyLinkedList #import
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -11,11 +8,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        self.size = 0
         self.limit = limit
-        self.dll = DoublyLinkedList() # holds key-value entries in the correct order 
-        self.storage = {} # provides fast access to every node in the cache
-
+        self.size = 0 
+        self.order = DoublyLinkedList()
+        self.storage = dict()
     """
     Retrieves the value associated with the given key. Also
     needs to move the key-value pair to the end of the order
@@ -24,13 +20,12 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        if key in self.storage:
-            element = self.storage[key]
-            self.dll.move_to_front(element)
-            return self.dll.head.value
+        if key in self.storage: # if key is in dictionary
+            node = self.storage[key] #extract node from storage at the key
+            self.order.move_to_end(node)#move the key to end of the order
+            return node.value[1] #return value of the value of the node
         else:
             return None
-
     """
     Adds the given key-value pair to the cache. The newly-
     added pair should be considered the most-recently used
@@ -42,34 +37,19 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        if self.size < self.limit:
-            # if size greater than limit
-            # add value to head of dll
-
-            self.dll.add_to_head(value)
-            self.storage[key] = self.dll.head
-            self.size += 1
-        elif key in self.storage:
-            # if key is already in storage
-            # delete current node with that key
-            # add new value to head of dll
-            # set head node to key
-
-            node = self.storage[key]
-            self.dll.delete(node)
-            self.dll.add_to_head(value)
-            self.storage[key] = self.dll.head
-        else:
-            # remove tail key from storage
-            # else remove least recent used element
-            # make new insertion the most recent
-
-            el = ''
-            for k,v in self.storage.items():
-                if v == self.dll.tail:
-                    el = k
-            del self.storage[el]
-
-            self.dll.remove_from_tail()
-            self.dll.add_to_head(value)
-            self.storage[key] = self.dll.head
+        #create a node if key is not found
+        #move node to front if key found
+        #if full remove last node from linked list and disctionary
+        if key in self.storage: # if key is in storage/dict
+            node = self.storage[key] #extract the node from storage
+            node.value= (key, value) #set node to kvp
+            self.order.move_to_end(node) #call the move to end from dll
+            return
+        if self.size == self.limit: #if its same as limit(10)
+            del self.storage[self.order.head.value[0]] #delete the storage at the heads value key
+            self.order.remove_from_head() #remove from head
+            self.size -= 1   #decrement size
+        #if both cases are false
+        self.order.add_to_tail((key, value))
+        self.storage[key] = self.order.tail
+        self.size += 1 #increment size
